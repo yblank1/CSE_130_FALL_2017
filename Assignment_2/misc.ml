e7768f99f9c12a371dab34a684b3266c42288ad5
Yitzchak Blank <blanco7258@gmail.com>
2017-10-13 14:43:20 -0700

    removeDuplicates function works - pending further testing.

--- Assignment_2/misc.ml
+++ Assignment_2/misc.ml
@@ -20,14 +20,32 @@ let rec assoc (d,k,l) = match l with
 
 
 (* fill in the code wherever it says : failwith "to be written" *)
+
+(* 
+TODO -- Proper header 
+Parameter - l is the list which will have duplicates removed from
+
+within the helper function
+seen is going to hold the list that will remain - ie there are no duplicates
+rest is obviously the rest of the list that has not yet been processed
+
+IDEA:
+Reverse the list. For each head of the list, run List.mem between h and t. If
+true, then discard a. Otherwise, append it to rest. Once done, reverse the seen
+list to recover original order... Note the last part is already done...  
+
+
+This works but seems inefficient. I added the first 3 List.rev calls. Should 
+rethink the logic here... Otherwise it works pending further testing --- TODO 
+*)
 let removeDuplicates l = 
   let rec helper (seen,rest) = 
-      match rest with 
-        [] -> seen
+      match List.rev(rest) with 
+        [] -> List.rev(seen)
       | h::t -> 
-        let seen' = failwith "to be written" in
-        let rest' = failwith "to be written" in 
-	  helper (seen',rest') 
+        let seen' = if List.mem h t then seen else h::seen in
+        let rest' = t in 
+	  helper (seen',List.rev(rest')) 
   in
       List.rev (helper ([],l))
 
