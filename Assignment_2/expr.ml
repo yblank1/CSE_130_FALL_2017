a5479e56a5567e2cb6c56eeb1f3076de426cc2b3
Yitzchak Blank <blanco7258@gmail.com>
2017-10-16 23:21:41 -0700

    Eval mostly implemented - avg not working - pending check on ieng6

--- Assignment_2/expr.ml
+++ Assignment_2/expr.ml
@@ -21,14 +21,15 @@ type expr =
   | Times    of expr * expr
   | Thresh   of expr * expr * expr * expr	
 
-let rec exprToString e = match e with 
+let rec exprToString e = 
+	match e with 
 	| VarX 			  -> "x"
 	| VarY 			  -> "y"
 	| Sine e 		  -> "sin(pi*" ^ exprToString e ^ ")" 
 	| Cosine e 		  -> "cos(pi*" ^ exprToString e ^ ")" 
 	| Average (e1, e2) 	  -> "((" ^ exprToString e1 ^ "+" ^ exprToString e2 ^ ")/2)" 
 	| Times (e1, e2) 	  -> exprToString e1 ^ "*" ^ exprToString e2 
-	| Thresh (e1, e2, e3, e4) -> "(" ^ exprToString e1 ^ "<" ^ exprToString e2 ^ "?" ^ exprToString e3 ^ ":" ^ exprToString e4 ^ ")" 
+	| Thresh (e1, e2, e3, e4) -> "(" ^ exprToString e1 ^ "<" ^ exprToString e2 ^ "?" ^ exprToString e3 ^ ":" ^ exprToString e4 ^ ")";; 
 	
 
 (* build functions:
@@ -47,7 +48,17 @@ let buildThresh(a,b,a_less,b_less) = Thresh(a,b,a_less,b_less)
 
 let pi = 4.0 *. atan 1.0
 
-let rec eval (e,x,y) = failwith "to be written"
+let rec eval (e,x,y) = 
+	match e with
+	| VarX 			  -> x
+	| VarY			  -> y 
+	| Sine e 		  -> sin(pi *. (eval (e, x, y)))   
+	| Cosine e 		  -> cos(pi *. (eval (e, x, y)))
+	| Average (e1, e2)	  -> eval(e1, x, y) +. eval (e2,x,,y) /. 2.0  
+	| Times (e1, e2)	  -> ((eval (e1,x,y)) *. (eval (e2,x,y)))
+	| Thresh (e1, e2, e3, e4) -> if (eval (e1,x,y)) < (eval (e2,x,y)) then (eval (e3,x,y)) else (eval (e4,x,y))
+	| _ -> x 
+;;	 
 
 (* (eval_fn e (x,y)) evaluates the expression e at the point (x,y) and then
  * verifies that the result is between -1 and 1.  If it is, the result is returned.  
