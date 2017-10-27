330a7af85ba2c70596542661efd42268eb2bfc2e
Yitzchak Blank <blanco7258@gmail.com>
2017-10-27 13:09:16 -0700

    Implemented more functions. BigAdd is not yet functional

--- Assignment_3/misc.ml
+++ Assignment_3/misc.ml
@@ -40,13 +40,13 @@ let sqsum xs =
   let f a x = a+(x*x) in
   let base = 0 in
     List.fold_left f base xs
-
-let pipe fs = failwith "to be implemented"  
-(*
-  let f a x = (fun z -> (x a))  in
+;;
+let pipe fs = 
+  let f a x = (fun z -> (x (a z)))  in
   let base = (fun y -> y)  in  
     List.fold_left f base fs
-*)
+;;
+
 let rec sepConcat sep sl = match sl with 
   | [] -> ""
   | h :: t -> 
@@ -54,29 +54,50 @@ let rec sepConcat sep sl = match sl with
       let base = h in
       let l = List.map (fun x -> sep ^ x) t in
         List.fold_left f base l
-
-let stringOfList f l = failwith "to be implemented"
+;;
+let stringOfList f l = 
+	let l1 = List.map f l  in 
+	"[" ^ sepConcat "; " l1 ^"]";;	
 
 (*****************************************************************)
 (******************* 2. Big Numbers ******************************)
 (*****************************************************************)
 
-let rec clone x n = failwith "to be implemented" 
+let rec clone x n = match n > 0 with 
+	| true-> x :: clone x (n-1) 
+	| false ->  []
+;; 
 
-let rec padZero l1 l2 = failwith "to be implemented"
 
-let rec removeZero l = failwith "to be implemented"
+let rec padZero l1 l2 = 
+	let lengthDiff = (List.length l1) - (List.length l2) in 
+	match (lengthDiff > 0) with 
+	| true -> (l1, List.append (clone 0 lengthDiff)  l2)
+	| false -> (List.append(clone 0 (List.length l2 - List.length l1)) l1, l2) 
+;;
 
+
+let rec removeZero l = match l with 
+	| [] -> []
+	| h::t -> match (h == 0) with 
+		| true -> removeZero t
+		| false -> l
+;;
+
+
+(* TODO remove List.hd - this is not allowed *)
 let bigAdd l1 l2 = 
   let add (l1, l2) = 
-    let f a x = failwith "to be implemented" in
-    let base = failwith "to be implemented" in
-    let args = failwith "to be implemented" in
+    let f a x = let (a1, a2) = a in 
+	let (x1, x2) = x in 
+	((x1 + x2 + a1) /10, ((x1 + x2 + a1) mod 10) :: a2) in   
+    let base = (0, []) in
+    let args = (l1, l2) in
     let (_, res) = List.fold_left f base args in
       res
   in 
     removeZero (add (padZero l1 l2))
-
+;;
 let rec mulByDigit i l = failwith "to be implemented"
 
 let bigMul l1 l2 = 
