c579fef64732117c9c8e51dd6b0f0cee28dc4c12
Yitzchak Blank <blanco7258@gmail.com>
2017-11-28 21:54:48 -0800

    Implemented Part c

--- Assignment_6/vector.py
+++ Assignment_6/vector.py
@@ -2,12 +2,12 @@ from misc import Failure
 
 class Vector(object):
 
-    """ Constructor for Vector
+    def __init__(self, arg):
+        """ Constructor for Vector
         Takes either an int or long (or a subclass of them and creates an empty
         vector of that length. Also takes a sequence and creates a vector with the
-        elements of that sequence. """ 
-    def __init__(self, arg):
-    
+        elements of that sequence. 
+        """ 
     # Case 1: it is of type int, long, or a class derived from those
         if isinstance(arg, (int, long)):
            
@@ -38,8 +38,8 @@ class Vector(object):
                 Vector constructor")
 
 
-    """ Function to represent elements in vector as a list """
     def __repr__(self):
+        """ Returns a string representing elements in vector as a list """
         ret_string = "Vector(["
         if self.length > 0:
             ret_string += str(self.vect_list[0])
@@ -51,10 +51,33 @@ class Vector(object):
         return ret_string
 
    
-    """ Function that returns the length of the vector """
     def __len__(self):
+        """ Function that returns the length of the vector """
         return self.length
 
     def __iter__(self):
+        """ Returns an iterator for the Vector object """ 
         return self.vect_list.__iter__()
- 
+
+    def __add__(self, nextSeq):
+        """ This function overloads the + to work component-wise on a vector and
+        any other sequence """
+        # First check that nextSeq is of the same length - if not raise error
+        if len(nextSeq) != len(self):
+            raise ValueError ("Error! All arguments for vector addition must be\
+                of same length")
+        else:
+            new_list = []
+            for i in self:
+                new_list.append(i) 
+            for i, j in enumerate(nextSeq):
+               new_list[i] += j
+            return Vector(new_list)
+
+    def __radd__(self, nextSeq):
+        return self.__add__(nextSeq) 
+
+    def __iadd__(self, nextSeq):
+        new_list = self.__add__(nextSeq)
+        self.vect_list = new_list.vect_list
+        return self 
